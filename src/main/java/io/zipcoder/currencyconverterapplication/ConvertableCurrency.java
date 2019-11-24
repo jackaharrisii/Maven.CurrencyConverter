fba2b1bc3ac8bbcb3672603567b86b35c62a46d8
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-24 16:19:54 -0500

    all tests pass

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -1,7 +1,17 @@
 package io.zipcoder.currencyconverterapplication;
 
+import io.zipcoder.currencyconverterapplication.currencies.UniversalCurrency;
+
 public interface ConvertableCurrency {
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+        return currencyType.getRate() / CurrencyType.getTypeOfCurrency(this).getRate();
     }
 }
+
+// returns rate of currency in () divided by rate of this.currency
+// e.g., US Dollars to Universal Currency
+// US_DOLLAR.convert(UNIVERSAL_CURRENCY);
+// UNIVERSAL_CURRENCY rate / US_DOLLAR rate
+// 1.00 / 2.00
+// 1/2 or 50%
+// so $10 US -> $5 Universal Currency
